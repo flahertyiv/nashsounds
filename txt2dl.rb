9aad79eac7c95701366cc56ef4fbd522251eb059
Will Flaherty <will@seatgeek.com>
2014-05-19 11:07:21 -0400

    Updated MAT Links

--- txt2dl.rb
+++ txt2dl.rb
@@ -5,7 +5,7 @@ require 'shotgun'
  
 get '/pitt' do
    twiml = Twilio::TwiML::Response.new do |r|
-   		r.Message "There's just one last step to enter SeatGeek’s Pirates ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://www.seatgeek.com/m"
+   		r.Message "There's just one last step to enter SeatGeek’s Pirates ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://goo.gl/oBbUeP"
    	end
    	twiml.text
 end
@@ -19,14 +19,14 @@ end
 
 get '/stl' do
    twiml = Twilio::TwiML::Response.new do |r|
-   		r.Message "There's just one last step to enter SeatGeek’s Cards ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://www.seatgeek.com/m"
+   		r.Message "There's just one last step to enter SeatGeek’s Cards ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://goo.gl/bXCU3D"
    	end
    	twiml.text
 end
 
 get '/cincy' do
    twiml = Twilio::TwiML::Response.new do |r|
-   		r.Message "There's just one last step to enter SeatGeek’s Reds ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://www.seatgeek.com/m"
+   		r.Message "There's just one last step to enter SeatGeek’s Reds ticket giveaway! Download the FREE SeatGeek app here to finalize your entry --> http://goo.gl/86SqyX"
    	end
    	twiml.text
 end
